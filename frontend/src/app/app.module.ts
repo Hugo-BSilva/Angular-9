aba8485ba5a48e21ca7bd995892c9cfe5cda4cf1
hugo-bsilva <09silvahugo@gmail.com>
2022-03-17 23:53:07 -0300

    Snack-bar da página de cadastro de produtos
    Basicamente um pop-up que aparece para demonstrar se foi tudo ok ou não.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -15,6 +15,7 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {HomeComponent} from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
@@ -42,7 +43,8 @@ import { ForDirective } from './directives/for.directive';
     MatSidenavModule,
     MatListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductService } from '../product.service';
+
 
 @Component({
   selector: 'app-product-create',
@@ -6,16 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-create.component.css']
 })
 export class ProductCreateComponent implements OnInit {
-  
-  propLegal = "qualquer"
 
-  constructor() { }
+  constructor(
+    private productService: ProductService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    
   }
 
-  fazerAlgo(): void {
-    console.log('Fazendo algo')
+  createProduct(): void {
+    this.productService.showMessage('Produto criado com sucesso !');
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+    this.productService.showMessage('Operação Cancelada !');
   }
 
 }
